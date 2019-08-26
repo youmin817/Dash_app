963d3976c640800fa81147ecf95561431160af86
Jake Lee <37559775+youmin817@users.noreply.github.com>
2019-08-25 21:29:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,53 @@
-print("Hi")
-      
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+########### Set up the chart
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
+
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name='IBU',
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name='ABV',
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = 'Beer Comparison'
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+########### Display the chart
+
+app = dash.Dash()
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1('Flying Dog Beers'),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
+    ),
+    html.A('Code on Github', href='https://github.com/austinlasseter/flying-dog-beers'),
+    html.Br(),
+    html.A('Data Source', href='https://www.flyingdog.com/beers/'),
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server()
+
