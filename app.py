63a78f8f9a41744f93abd522da046752927811b7
Jake Lee <37559775+youmin817@users.noreply.github.com>
2019-08-25 20:54:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,2 @@
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
-
-
-app.layout = html.Div([
-
-    dcc.Markdown(children=top_markdown_text),
-
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+print("Hi)
+      
